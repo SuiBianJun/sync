1e182d71ea2d56fa2d745412743c630da8225f06
dlg <2623394162@qq.com>
2019-10-28 19:47:35 +0800

    add webpack,iview

---
+++ src/index.js
@@ -0,0 +1,11 @@
+
+
+import $ from "jquery"
+
+import Vue from '../node_modules/vue/dist/vue'
+
+var vm = new Vue({
+
+    el: "#page"
+
+});
---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const webpack = require('webpack');
+const htmlWebpackPlugin = require('html-webpack-plugin');
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+
+module.exports = {
+	mode: 'development',
+	entry: './src/index.js',
+	output: {
+		filename: 'bundle.js',
+		path: path.resolve(__dirname, 'dist'),
+	},
+	plugins: [
+		// 热更新，只更新修改的部分，而不是整个文件
+		new webpack.HotModuleReplacementPlugin(),
+		new htmlWebpackPlugin({
+			// 指定打开的页面路径
+		  template: path.join(__dirname, './httpserver/html/home/home.html')
+		}),
+		new VueLoaderPlugin()
+	  ],
+	devServer: {
+		contentBase: './html',
+		hot: true,
+		port: 3000
+	},
+	module: {
+		rules: [
+			// test正则不要添加符号
+			{test: /\.css$/, use: ['style-loader', 'css-loader']},
+			{test: /\.(jpg|png)$/, use: ['url-loader?limit=3000&name=[hash:8]-[name].[ext]']},
+			{test: /\.js$/, use: ['babel-loader'], exclude: /node_modules/ },
+			{test: /\.vue$/, use: ['vue-loader']},
+		]
+	}
+};
